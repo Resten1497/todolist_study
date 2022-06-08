fde492d832ed80333be9a3b7e80e77b16ed4e6e3
Resten1497 <saup19@naver.com>
2022-06-08 09:06:02 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle } from "styled-components";
+
+import Header from "./components/Header.js";
+import Todo from "./components/todo/Todo";
+
+const GlobalStyle = createGlobalStyle`
+  *{  
+    box-sizing: border-box;
+  }
+  html {
+    font-size : 10px;
+  }
+  body {
+    width: 100%;
+    background-color : #F6FFDB;
+    margin: 0;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <GlobalStyle />
+      <Header />
+      <Todo />
+    </Wrapper>
   );
 }
 
+const Wrapper = styled.div`
+  @font-face {
+    font-family: "Pretendard-Regular";
+    src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff")
+      format("woff");
+    font-weight: 400;
+    font-style: normal;
+  }
+  font-family: "Pretendard-Regular";
+`;
 export default App;
---
+++ src/components/todo/Item.js
@@ -0,0 +1,63 @@
+import styled, { css } from "styled-components";
+import { useState, useRef } from "react";
+import { IoCloseOutline } from "react-icons/io5";
+
+export default function Item({ todoList, deleteTodo }) {
+  const todoLabel = useRef([]);
+  const checkbox = useRef([]);
+
+  // todo 삭제하기
+  const handleDeleteClick = (index) => {
+    const value = todoLabel.current[index].innerText;
+    deleteTodo(value);
+  };
+
+  const handleBoxClick = (index) => {
+    const { style } = todoLabel.current[index]; // 클릭한 todo의 style
+    const { checked } = checkbox.current[index];
+    // 체크하면 todo에 중간 줄 긋기 (toggle)
+    style.textDecoration = checked ? "line-through" : null;
+    style.color = checked ? "gray" : null;
+  };
+
+  return (
+    <TodoLists>
+      {todoList.map((todo, index) => (
+        <TodoList key={todo.id}>
+          <Checkbox
+            ref={(box) => (checkbox.current[index] = box)}
+            type="checkbox"
+            id={index}
+            onClick={() => handleBoxClick(index)}
+          />
+          <StyledLabel
+            ref={(label) => (todoLabel.current[index] = label)}
+            htmlFor={index}
+          >
+            {todo.value}
+          </StyledLabel>
+          <IoCloseOutline
+            size="2rem"
+            onClick={() => handleDeleteClick(index)}
+          />
+        </TodoList>
+      ))}
+    </TodoLists>
+  );
+}
+
+const TodoLists = styled.div`
+  margin-left: 0.5rem;
+`;
+const TodoList = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+`;
+const Checkbox = styled.input`
+  transform: scale(1.5);
+  margin: 0 1rem;
+`;
+const StyledLabel = styled.label`
+  margin-right: 0.5rem;
+`;
